d592a45517997b1a507b8f4d75ef5d3d2acbd8ff
iamchokerman <kuzminivan51@gmail.com>
2022-03-02 16:26:00 +0100

    feature: disconnect and add

--- judicator.py
+++ judicator.py
@@ -1,7 +1,7 @@
 import discord
 import secrets
 from discord.ext import commands
-
+import platform # For stats
 
 intents = discord.Intents.all()
 
@@ -10,7 +10,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"-----\nLogged in as: {bot.user.name} : {bot.user.id}\n-----\nMy current prefix is: -\n-----")
+    print(f"-----\nLogged in as: {bot.user.name} : {bot.user.id}\n-----\nMy current prefix is: !\n-----")
 
 
 @bot.event
@@ -49,6 +49,54 @@ async def on_raw_reaction_add(payload):
 async def testing(ctx: commands.Context):
     await ctx.send("I hate my life!")
 
+@bot.command(name='hi', aliases=['hello','yo'])
+async def _hi(ctx):
+    """
+    A simple command which says hi to the author.
+    """
+    await ctx.send(f"Hi {ctx.author.mention}!")
 
+@bot.command(aliases=['disconnect', 'close', 'stopbot'])
+@commands.is_owner()
+async def logout(ctx):
+    """
+    If the user running the command owns the bot then this will disconnect the bot from discord.
+    """
+    await ctx.send(f"Hey {ctx.author.mention}, I am now logging out :wave:")
+    await bot.logout()
+
+@logout.error
+async def logout_error(ctx, error):
+    """
+    Whenever the logout command has an error this will be tripped.
+    """
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send("Hey! You lack permission to use this command as you do not own the bot.")
+    else:
+        raise error
+
+@bot.command()
+async def stats(ctx):
+    """
+    A usefull command that displays bot statistics.
+    """
+    pythonVersion = platform.python_version()
+    dpyVersion = discord.__version__
+    serverCount = len(bot.guilds)
+    memberCount = len(set(bot.get_all_members()))
+
+    embed = discord.Embed(title=f'{bot.user.name} Stats', description='\uFEFF', colour=ctx.author.colour, timestamp=ctx.message.created_at)
+
+    #embed.add_field(name='Bot Version:', value=bot.version)
+    embed.add_field(name='Python Version:', value=pythonVersion)
+    embed.add_field(name='Discord.Py Version', value=dpyVersion)
+    embed.add_field(name='Total Guilds:', value=serverCount)
+    embed.add_field(name='Total Users:', value=memberCount)
+    embed.add_field(name='Bot Developers:', value="<@503505263119040522>,<@453579828281475084>")
+
+    embed.set_footer(text=f"{bot.user.name}")
+    embed.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
+
+    await ctx.send(embed=embed)
 
 bot.run(secrets.OPEN_SOURCE_TOKEN)
