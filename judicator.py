67869ba375249aaefe1d425249109b1038b7d44f
Johnpier <nicropro@gmail.com>
2022-03-02 14:33:25 +0100

    fix: fixed reaction role feature

--- judicator.py
+++ judicator.py
@@ -1,21 +1,11 @@
 import discord
 import secrets
 from discord.ext import commands
-from discord import Game
+
 
 intents = discord.Intents.all()
 
-help_command = commands.DefaultHelpCommand(
-    no_category='Commands'
-)
-
-bot = commands.Bot(command_prefix='!', description='Testing Commands',
-                   help_command=help_command, intents=intents)
-
-
-@bot.command()
-async def testing(ctx: commands.Context):
-    await ctx.send("I hate my life!")
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
 @bot.event
@@ -23,47 +13,41 @@ async def on_ready():
     print("Logged in as "+str(bot.user.name))
 
 
-class MyBot(commands.Bot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # ID of the message that can be reacted to to add/remove a role.
-        self.role_message_id = 947968073070161980
-        self.emoji_to_role = {
-            # ID of the role associated with unicode emoji '✅'.
-            discord.PartialEmoji(name='✅'): 917940661901209650,
-            # ID of the role associated with unicode emoji '❎'.
-            discord.PartialEmoji(name='❎'): 936351416430256128,
-        }
+@bot.event
+async def on_raw_reaction_add(payload):
+    """Gives a role based on a reaction emoji."""
+    # Make sure that the message the user is reacting to is the one we care about.
+    if payload.message_id != 947968073070161980:
+        return
 
-    @bot.event
-    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        """Gives a role based on a reaction emoji."""
-        # Make sure that the message the user is reacting to is the one we care about.
-        if payload.message_id != self.role_message_id:
-            return
+    guild = await bot.fetch_guild(payload.guild_id)
+    if guild is None:
+        # Check if we're still in the guild and it's cached.
+        return
 
-        guild = self.get_guild(payload.guild_id)
-        if guild is None:
-            # Check if we're still in the guild and it's cached.
-            return
+    if str(payload.emoji) == "✅":
+        role_id = 917940661901209650
+    elif str(payload.emoji) == "❎":
+        role_id = 936351416430256128
+    else:
+        role_id = None
 
-        try:
-            role_id = self.emoji_to_role[payload.emoji]
-        except KeyError:
-            # If the emoji isn't the one we care about then exit as well.
-            return
+    role = guild.get_role(role_id)
+    if role is None:
+        # Make sure the role still exists and is valid.
+        return
 
-        role = guild.get_role(role_id)
-        if role is None:
-            # Make sure the role still exists and is valid.
-            return
+    try:
+        # Finally, add the role.
+        await payload.member.add_roles(role)
+    except discord.HTTPException:
+        # If we want to do something in case of errors we'd do it here.
+        pass
 
-        try:
-            # Finally, add the role.
-            await payload.member.add_roles(role)
-        except discord.HTTPException:
-            # If we want to do something in case of errors we'd do it here.
-            pass
+
+@bot.command()
+async def testing(ctx: commands.Context):
+    await ctx.send("I hate my life!")
 
 
 bot.run(secrets.OPEN_SOURCE_TOKEN)
