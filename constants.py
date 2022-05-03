47885aadbb1437647dfa164b9e094524067110b6
johnpier <nicropro@gmail.com>
2022-05-03 19:45:55 +0200

    docs: added missing channel

--- constants.py
+++ constants.py
@@ -426,7 +426,7 @@ BLOCKED_CHANNELS = {
     'fop-solutions', 'ca-solutions', 'ds-solutions', 'main', 'test',
     'fc-bayern', 'sources-fop', 'sources-ds','sources-ca', 'matrix-test',
     'ARCHIVES', 'Important Information','testing room','moderators',
-    'Server Information','announcements','rules'
+    'Server Information','announcements','rules', 'shortcuts-second-semester'
 }
 
 BOT_COLORS = {
