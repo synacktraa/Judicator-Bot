664586532820080858d6603847540ec9044ec71c
johnpier <nicropro@gmail.com>
2022-06-09 21:29:39 +0200

    fix(constants.py): added new channel

--- logic/constants.py
+++ logic/constants.py
@@ -428,7 +428,7 @@ BLOCKED_CHANNELS = {
     'ARCHIVES', 'Important Information','testing room','moderators',
     'Server Information','announcements','rules', 'shortcuts-second-semester',
     'shortcuts-first-semester', 'holy-crushade',"Artwork & more", "sfw", "nsfw",
-    'la-hw', 'Holy Crushade', 'lecture-materials-webhook'
+    'la-hw', 'Holy Crushade', 'lecture-materials-webhook', 'music'
 }
 
 BOT_COLORS = {
